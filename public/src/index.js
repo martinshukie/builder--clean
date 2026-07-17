38e1d732c73119688fa40d8942592f22717744a6
martinshukie <martinshukie69@gmail.com>
2026-07-18 09:45:59 +1000

    Create index.js with React setup
    Initialize index.js with React imports and root element.

---
+++ public/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'; // remove or comment out if you don't add src/index.css
+import App from './App';
+const rootElement = document.getElementById('root');
